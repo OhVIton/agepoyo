93699dd1ac4962424fdd1f61fb10ddaa4ad89de1
OhVIton <vim.usenoarrows@gmail.com>
2025-05-17 19:35:44 +0000

    add linter

--- app/controllers/agepoyo_service_impl.py
+++ app/controllers/agepoyo_service_impl.py
@@ -1,4 +1,5 @@
 from gen.schema import schema_pb2_grpc
 
-class AgepoyoServiceImpl(schema_pb2_grpc.AgepoyoServiceServicer):
-    pass  # 実装は後で
+
+class AgepoyoServiceImpl(schema_pb2_grpc.AgepoyoServiceServicer):  # type: ignore[misc]
+    """gRPC AgepoyoService の実装クラス. 詳細は後で実装."""
--- app/main.py
+++ app/main.py
@@ -1,15 +1,18 @@
-import grpc
 from concurrent import futures
-from gen.schema import schema_pb2_grpc
-from controllers.agepoyo_service_impl import AgepoyoServiceImpl
 
-def serve():
+import grpc
+from controllers.agepoyo_service_impl import AgepoyoServiceImpl
+from gen.schema import schema_pb2_grpc
+
+
+def serve() -> None:
+    """gRPCサーバーを起動する."""
     server = grpc.server(futures.ThreadPoolExecutor(max_workers=10))
     schema_pb2_grpc.add_AgepoyoServiceServicer_to_server(AgepoyoServiceImpl(), server)
     server.add_insecure_port('[::]:50051')
     server.start()
-    print('gRPC server started on port 50051')
     server.wait_for_termination()
+
 
 if __name__ == '__main__':
     serve()
